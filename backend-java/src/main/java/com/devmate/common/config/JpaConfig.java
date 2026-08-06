13c29734ab394b032b35afe6580d60592fa9d3df
Muhammed Niyas <MuhammedNiyas2003@github.com>
2026-08-06 23:57:24 +0530

    feat: add BaseEntity and JPA auditing configuration

---
+++ backend-java/src/main/java/com/devmate/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.devmate.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
